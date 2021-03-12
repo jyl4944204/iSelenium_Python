732ea5b69c86352610d58708f9803155c5871c61
jyl4944204 <40823666+jyl4944204@users.noreply.github.com>
2021-03-12 14:53:10 +0800

    Update web_ut.py

--- test/web_ut.py
+++ test/web_ut.py
@@ -34,8 +34,8 @@ class ISelenium(unittest.TestCase):
         chrome_options = Options()
         if using_headless is not None and using_headless.lower() == 'true':
             print('使用无界面方式运行')
-            chrome_options.add_argument("--no-sandbox)
-            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--headless")
          
 
